71d196cd4adf8d056588e85b7a65d773df3aea9c
SASIPKP <35728494+SASIPKP@users.noreply.github.com>
2018-03-02 14:24:19 +0530

    Update number n.c

--- number n.c
+++ number n.c
@@ -2,22 +2,22 @@
 void main()
 {
 int n,n1,temp=0,c;
-printf("enter the number");
-scanf("%d",&n);
-c=n;
-while(n>0)
+printf("Enter the number");
+ scanf("%d",&n);
+  c=n;
+ while(n>0)
 {
-n1=n%10;
-temp=temp+(n1*n1*n1);
-n=n/10;
+ n1=n%10;
+  temp=temp+(n1*n1*n1);
+  n=n/10;
 }
 printf("%d",temp);
 if(temp==c)
 {
-printf("armstrong number");
+printf("Armstrong number");
 }
 else
 {
-printf("not an armstrong");
+printf("Not an armstrong");
 }
 }
